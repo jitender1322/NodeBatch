c6a660e5e62b65dae9a65f570f105da534ac8815
jitender1322 <jitendersarswat6@gmail.com>
2024-09-18 10:37:01 +0530

    practice

--- RestApi/controller/userCtl.js
+++ RestApi/controller/userCtl.js
@@ -1,6 +1,7 @@
 let users = require("../model/register")
 let bcrypt = require("bcrypt");
 let moment = require("moment");
+let jwt = require("jsonwebtoken");
 
 module.exports.signUp = async (req, res) => {
     let user = await users.findOne({ email: req.body.email });
@@ -17,8 +18,9 @@ module.exports.signUp = async (req, res) => {
 module.exports.checkUser = async (req, res) => {
     let user = await users.findOne({ email: req.body.email });
     if (user) {
-        if (bcrypt.compare(req.body.password, user.password)) {
-            res.status(200).json({ msg: "password is right" })
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            let token = await jwt.sign({ userData: user }, "rnw", { expiresIn: "1h" });
+            res.status(200).json({ msg: "password is right", tokenNum: token })
         } else {
             res.status(400).json({ msg: "password is wrong" })
         }
